9a0d0ba3167f017cb0e79f69e557a966f25fd8c5
akramthedev <seasonedwebdev@gmail.com>
2024-04-07 18:16:38 +0000

    Next Scope : user shall change his About Section and we shall display a button to create an about as well as a pop Up. Then Next Scope will be modifying the profile informations

--- client/src/Assets/AdminSymbol.jsx
+++ client/src/Assets/AdminSymbol.jsx
@@ -3,7 +3,7 @@ import './index.css';
 
 const AdminSymbol = () => {
   return (
-      <div className='Administrator' title='Administrator '>
+      <div className='Administrator' title='Moderator '>
         <svg  className='cc' width="18" height="18" viewBox="0 0 64 64" fill="none" role="presentation" aria-hidden="true" focusable="false" class="hi fc"><path d="M39.64 40.83L33.87 56.7a1.99 1.99 0 0 1-3.74 0l-5.77-15.87a2.02 2.02 0 0 0-1.2-1.2L7.3 33.88a1.99 1.99 0 0 1 0-3.74l15.87-5.77a2.02 2.02 0 0 0 1.2-1.2L30.12 7.3a1.99 1.99 0 0 1 3.74 0l5.77 15.87a2.02 2.02 0 0 0 1.2 1.2l15.86 5.76a1.99 1.99 0 0 1 0 3.74l-15.87 5.77a2.02 2.02 0 0 0-1.2 1.2z" fill="#FFC017"></path></svg>
       </div>
     )
